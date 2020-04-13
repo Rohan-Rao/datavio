18a72d64c847a69f3253344c62bf378443600f6d
Rohan-Rao <rohanrao317@gmail.com>
2020-04-13 21:31:01 +0530

    add db-migrate and create register table script

---
+++ migrations/sqls/20200413135704-createTableRegister-up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE register(
+    id INT(11) AUTO_INCREMENT,
+    email VARCHAR(30) NOT NULL UNIQUE,
+    user_password BLOB NOT NULL,
+    registered_with ENUM('DATAVIO', 'GOOGLE', 'FACEBOOK') NOT NULL,
+    is_email_verified BOOLEAN DEFAULT 0,
+    is_logged_in BOOLEAN DEFAULT 0,
+    last_logged_in DATETIME,
+    PRIMARY KEY (id)
+);
