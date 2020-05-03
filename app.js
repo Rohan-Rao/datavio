e2cf43c2af33648da73ab5a4816ce65276281d9f
Rohan-Rao <rohanrao317@gmail.com>
2020-05-03 22:09:23 +0530

    add bodyparser

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const handleError = require('./middlewares');
 const apiV1 = require('./api-routes/api-v1');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(bodyparser.json({ limit: '2mb' })); // middleware to parse JSON data, Content-Type: application/json
+app.use(bodyparser.urlencoded({ limit: '1mb', extended: false })); // middleware to parse form data, Content-Type: application/x-www-form-urlencoded
 
 app.get('/', async (req, res) => {
   res.send("DataVio API's are up and running.");
@@ -17,7 +21,7 @@ app.use('/api/v1', apiV1);
 
 /**
  * Register a global error handler here.
- * According to thr express docs, this should be the last middleware
+ * According to the express docs, this should be the last middleware
  * registered on the express instance.
  */
 app.use(handleError);
