bc2c256a7cec675e8464cdd6fccc40575026d323
Rohan-Rao <rohanrao317@gmail.com>
2020-05-03 22:27:37 +0530

    change column name from email to email_id

--- migrations/sqls/20200413135704-createTableRegister-up.sql
+++ migrations/sqls/20200413135704-createTableRegister-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE register(
     id INT(11) AUTO_INCREMENT,
-    email VARCHAR(30) NOT NULL UNIQUE,
+    email_id VARCHAR(30) NOT NULL UNIQUE,
     user_password BLOB NOT NULL,
     registered_with ENUM('DATAVIO', 'GOOGLE', 'FACEBOOK') NOT NULL,
     is_email_verified BOOLEAN DEFAULT 0,
